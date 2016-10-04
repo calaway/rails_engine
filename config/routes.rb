7f7d9ad9158dfee2ac9fa686c90615d7030f9ce5
Jean Joeris <joeris.peter@gmail.com>
2016-10-03 19:09:46 -0600

    Add index route and action for items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :invoices, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
+      resources :items, only: [:index]
     end
   end
 end
