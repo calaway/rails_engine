46229bf54719b0c0680a847d62844f428e385dd4
Jean Joeris <joeris.peter@gmail.com>
2016-10-03 19:37:05 -0600

    Add tests for searching invoices by merchant and customer id

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe "Invoice record API" do
     expect(response_invoice["customer_id"]).to eq(123)
     expect(response_invoice["merchant_id"]).to eq(123)
   end
+
+  it "finds one invoice by customer id" do
+    invoice = create(:invoice, customer_id: 123, merchant_id: 555)
+
+    get "/api/v1/invoices/find?customer_id=123"
+    response_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoice["customer_id"]).to eq(123)
+    expect(response_invoice["merchant_id"]).to eq(555)
+  end
+
+  it "finds one invoice by merchant id" do
+    invoice = create(:invoice, customer_id: 123, merchant_id: 555)
+
+    get "/api/v1/invoices/find?merchant_id=555"
+    response_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoice["customer_id"]).to eq(123)
+    expect(response_invoice["merchant_id"]).to eq(555)
+  end
 end
