c4bc256321b3c41ce3bc3b8de3aea4e39a1c6e0f
Jean Joeris <joeris.peter@gmail.com>
2016-10-04 08:25:36 -0600

    Add invoice items find all test

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -44,4 +44,54 @@ RSpec.describe "Invoice record API" do
     expect(response_invoice["customer_id"]).to eq(123)
     expect(response_invoice["merchant_id"]).to eq(555)
   end
+
+  it "finds one invoice by status" do
+    invoice = create(:invoice, status: "test status", merchant_id: 555)
+
+    get "/api/v1/invoices/find?merchant_id=555"
+    response_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoice["status"]).to eq("test status")
+    expect(response_invoice["merchant_id"]).to eq(555)
+  end
+
+  it "finds many invoices by merchant id" do
+    create(:invoice)
+    create(:invoice, customer_id: 123, merchant_id: 555)
+    create(:invoice, customer_id: 321, merchant_id: 555)
+
+    get "/api/v1/invoices/find_all?merchant_id=555"
+    response_invoices = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoices.first["customer_id"]).to eq(123)
+    expect(response_invoices.last["customer_id"]).to eq(321)
+  end
+
+  it "finds many invoices by customer id" do
+    create(:invoice)
+    create(:invoice, customer_id: 123, merchant_id: 555)
+    create(:invoice, customer_id: 123, merchant_id: 444)
+
+    get "/api/v1/invoices/find_all?customer_id=123"
+    response_invoices = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoices.first["merchant_id"]).to eq(555)
+    expect(response_invoices.last["merchant_id"]).to eq(444)
+  end
+
+  it "finds many invoices by status" do
+    create(:invoice)
+    create(:invoice, customer_id: 123, status: "Completed")
+    create(:invoice, customer_id: 321, status: "Completed")
+
+    get "/api/v1/invoices/find_all?status=Completed"
+    response_invoices = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(response_invoices.first["customer_id"]).to eq(123)
+    expect(response_invoices.last["customer_id"]).to eq(321)
+  end
 end
