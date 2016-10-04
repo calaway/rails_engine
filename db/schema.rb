fa84c98e3450838915de78638fc355f9adfbbc86
Jean Joeris <joeris.peter@gmail.com>
2016-10-03 19:05:21 -0600

    Add item model
    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004005156) do
+ActiveRecord::Schema.define(version: 20161004010450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20161004005156) do
     t.integer  "customer_id"
     t.integer  "merchant_id"
     t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :item do
+    name "MyString"
+    description "MyText"
+    unit_price 1
+    merchant_id 1
+  end
+end
