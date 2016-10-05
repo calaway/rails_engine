dd696cf8d8f3bdca8f123322b02c2e3aef02ecf4
Calaway <Christopher.Calaway@gmail.com>
2016-10-05 15:07:15 -0600

    Add factory for invoice with invoice items

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -12,5 +12,14 @@ FactoryGirl.define do
         create_list(:transaction, evaluator.transactions_count, invoice: invoice)
       end
     end
+
+    factory :invoice_with_invoice_items do
+      transient do
+        invoice_items_count 2
+      end
+      after(:create) do |invoice, evaluator|
+        create_list(:invoice_item, evaluator.invoice_items_count, invoice: invoice)
+      end
+    end
   end
 end
