efe9a5f27e470d3beb22feebedb61a9e91d4449c
Jean Joeris <joeris.peter@gmail.com>
2016-10-05 09:28:10 -0600

    Change text on items and merchants to be case insensitive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005151636) do
+ActiveRecord::Schema.define(version: 20161005152534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20161005151636) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
+    t.citext   "name"
+    t.citext   "description"
     t.integer  "unit_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20161005151636) do
   end
 
   create_table "merchants", force: :cascade do |t|
-    t.string   "name"
+    t.citext   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
