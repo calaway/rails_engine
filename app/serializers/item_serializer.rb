368937278f0a75d138bfaeb2258dcabb5322574f
Calaway <Christopher.Calaway@gmail.com>
2016-10-05 14:26:06 -0600

    Add invoice item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,6 +2,6 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :unit_price, :merchant_id
 
   def unit_price
-    "%.2f"%(object.unit_price/100.0)
+    "%.2f" % (object.unit_price / 100.0)
   end
 end
