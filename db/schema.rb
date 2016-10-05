32135bab14f28efa34ba596dea8ee1592fc70d8f
Jean Joeris <joeris.peter@gmail.com>
2016-10-05 09:36:42 -0600

    Change transactions to use citext

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005152534) do
+ActiveRecord::Schema.define(version: 20161005152829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20161005152534) do
   create_table "transactions", force: :cascade do |t|
     t.integer  "invoice_id"
     t.datetime "credit_card_expiration_date"
-    t.string   "result"
+    t.citext   "result"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "credit_card_number"
