54a19be234ea405fdc28aa273342be24dbb218e4
Jean Joeris <joeris.peter@gmail.com>
2016-10-04 20:35:18 -0600

    Add index action and appropriate relations for customer transactions

--- app/controllers/api/v1/customer_transactions_controller.rb
+++ app/controllers/api/v1/customer_transactions_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::CustomerTransactionsController < ApplicationController
+  def index
+    customer = Customer.find(params[:customer_id])
+    render json: customer.transactions
+  end
 end
