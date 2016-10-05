3d647b3cd0b308863a25fd5cf382dbd18bbbaf91
Jean Joeris <joeris.peter@gmail.com>
2016-10-05 17:04:56 -0600

    Add item with invoice items factory and item has many invoice items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,5 +4,14 @@ FactoryGirl.define do
     name "MyString"
     description "MyText"
     unit_price 1
+
+    factory :item_with_invoice_items do
+      transient do
+        invoice_items_count 2
+      end
+      after(:create) do |item, evaluator|
+        create_list(:invoice_item, evaluator.invoice_items_count, item: item)
+      end
+    end
   end
 end
