4965d108cb2b843f0869d9722889a0122d915297
Jean Joeris <joeris.peter@gmail.com>
2016-10-05 19:11:19 -0600

    Add revenue method and need relations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,11 +2,22 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.top_item_merchants(num)
-    group(:id).
     joins(:invoice_items).
+    group('merchants.id').
     order('sum(quantity) desc').
     limit(num)
+  end
+
+  def revenue
+    successful_transactions.
+    joins(:invoice_items).
+    sum('invoice_items.quantity * invoice_items.unit_price')
+  end
+
+  def successful_transactions
+    transactions.where(result: "success")
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   default_scope { order('id asc') }
-  
+
   belongs_to :invoice
+  has_many :invoice_items, through: :invoice
 end
