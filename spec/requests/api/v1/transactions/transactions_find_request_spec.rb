781f6c8cd9d79df9a2d79ad65adeacc476da218a
Calaway <Christopher.Calaway@gmail.com>
2016-10-06 14:35:04 -0600

    Refactor application record to remove methods that are no longer used.

--- spec/requests/api/v1/transactions/transactions_find_request_spec.rb
+++ spec/requests/api/v1/transactions/transactions_find_request_spec.rb
@@ -43,24 +43,4 @@ describe "Transactions find API" do
 
     expect(response_transaction["credit_card_number"]).to eq(cc)
   end
-
-  xit "finds one transaction by creation time" do
-    create(:transaction)
-    transaction = create(:transaction, credit_card_number: "4654405418249632")
-
-    get "/api/v1/transactions/find?created_at=#{transaction.created_at}"
-    response_transaction = JSON.parse(response.body)
-
-    expect(response_transaction["name"]).to eq("Turing Safe Cracking")
-  end
-
-  xit "finds one transaction by update time" do
-    create(:transaction)
-    transaction = create(:transaction, credit_card_number: "4654405418249632")
-
-    get "/api/v1/transactions/find?created_at=#{transaction.updated_at}"
-    response_transaction = JSON.parse(response.body)
-
-    expect(response_transaction["name"]).to eq("Turing Safe Cracking")
-  end
 end
