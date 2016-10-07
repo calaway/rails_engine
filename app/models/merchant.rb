808d6b8590de3fd2403aa2febf85c99546aa84dc
Calaway <Christopher.Calaway@gmail.com>
2016-10-06 21:47:31 -0600

    Work in progress on customers_with_pending_invoices.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -50,4 +50,25 @@ class Merchant < ApplicationRecord
     group('customers.id').
     order('count(transactions.*) desc').first
   end
+
+  def customers_with_pending_invoices
+    SELECT DISTINCT(customers.id), customers.*
+    FROM customers
+    INNER JOIN invoices     ON invoices.customer_id    = customer.id
+    INNER JOIN transactions ON transactions.invoice_id = invoices.id
+    WHERE invoices.merchant_id = 17
+    WHERE transactions.result = "failed"
+
+    SELECT "customers".*
+    FROM "customers"
+    INNER JOIN "invoices" ON "invoices"."customer_id" = "customers"."id"
+    INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id"
+    WHERE "invoices"."merchant_id" = $1
+    AND "transactions"."result" = $2  [["merchant_id", 17], ["result", "failed"]]
+
+    Customer.
+    joins(:transactions).
+    where(invoices: { merchant_id: 17 }).
+    where(transactions: { result: "failed" })
+  end
 end
