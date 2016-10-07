67ab32b51e12aa1bb98e6bb8c5678c399aaa4965
Jean Joeris <joeris.peter@gmail.com>
2016-10-06 21:33:21 -0600

    Add controller for items with most sales

--- app/controllers/api/v1/items/most_revenue_controller.rb
+++ app/controllers/api/v1/items/most_revenue_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::Items::MostRevenueController < ApplicationController
   def index
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
+    # Item.joins(:invoice_items).joins("INNER JOIN transactions ON transactions.invoice_id = invoice_items.invoice_id").where("transactions.result = 'success'").group("items.id").order("sum(invoice_items.quantity * invoice_items.unit_price) desc").limit(1)
+    # require "pry"; binding.pry
+    render json: Item.most_revenue(params[:quantity])
   end
 end
