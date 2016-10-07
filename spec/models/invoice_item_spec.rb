5ceff223cf7988c2bd2a485c97e597c4d408b75c
Calaway <Christopher.Calaway@gmail.com>
2016-10-07 09:37:03 -0600

    Add tests to verify active record associations in invoice items.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
   it { should belong_to(:invoice) }
+  it { should belong_to(:item) }
+  it { should have_many(:transactions) }
 end
