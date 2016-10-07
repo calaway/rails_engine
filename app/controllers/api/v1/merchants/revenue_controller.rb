f384ff7985ed41e5177de3d894b66e31cbc085f1
Calaway <Christopher.Calaway@gmail.com>
2016-10-06 19:48:01 -0600

    Refactor test for merchant revenue by date for readability.

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    # render json: Merchant.find(params[:id]).revenue
-    # render json: "{\"revenue\":\"#{Merchant.find(params[:id]).revenue / 100.0}\"}"
-    render json: Merchant.find(params[:id]).revenue, serializer: MerchantRevenueSerializer
+    if params[:date]
+      render json: Merchant.find(params[:id]).revenue_by_date(params[:date]), serializer: MerchantRevenueSerializer
+    else
+      render json: Merchant.find(params[:id]).revenue, serializer: MerchantRevenueSerializer
+    end
   end
 end
